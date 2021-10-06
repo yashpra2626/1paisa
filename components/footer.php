fa2016ea42e835535135b9b490cdbea8c8d5ded6
raj patel <kakhilesh79@gmail.com>
2021-10-06 14:45:58 +0530

    new customway

---
+++ components/footer.php
@@ -0,0 +1,55 @@
+
+<footer id="footer" class="bg-one">
+  <div class="top-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <h3>about</h3>
+          <p>Applying for a loan is not a complicated process. You should be particularly careful about the fact that you provide banks with all the genuine documents.</p>
+        </div>
+        <!-- End of .col-sm-3 -->
+
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <ul>
+            <li><h3>Our Services</h3></li>
+            <li><a href="#">Mobile Loan</a></li>
+            <li><a href="#">Shop Loan</a></li>
+            <li><a href="#">House Loan</a></li>
+            <li><a href="#">Personal Loan</a></li>
+            <li><a href="#">Vehical Loan</a></li>
+            </ul>
+	    <br>
+        </div>
+        <!-- End of .col-sm-3 -->
+
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <ul>
+            <li><h3>Quick Links</h3></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+          </ul>
+	  <br>
+        </div>
+        <!-- End of .col-sm-3 -->
+
+        <div class="col-sm-3 col-md-3 col-lg-3">
+          <ul>
+            <li><h3>Connect with us Socially</h3></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Youtube</a></li>
+            <li><a href="#">Pinterest</a></li>
+          </ul>
+        </div>
+        <!-- End of .col-sm-3 -->
+
+      </div>
+    </div> <!-- end container -->
+  </div>
+  <div class="footer-bottom">
+    <h5>Copyright 2021. All rights reserved.</h5>
+    <h6>Design and Developed by <a href="">Read India Grow India Pvt. Ltd.</a></h6>
+  </div>
+</footer> <!-- end footer -->
---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/footer.css">
+
+    <title>1Paisa - Homepage</title>
+  </head>
+  <body>
+   
+<!-- navbar -->
+ <?php include "./components/header.php"; ?>
+
+ <!-- slider home page -->
+ <?php include "./components/slider.php"; ?>
+
+
+<!-- main content start-->
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum, laboriosam ipsa quasi accusantium repudiandae porro? Saepe natus nostrum, dolore optio eaque eius? Consectetur aperiam nobis voluptates sunt possimus ipsum quia corrupti vel repellendus excepturi accusantium minima reiciendis, recusandae soluta eius natus earum quidem aut neque maiores illo? Deserunt corrupti autem perspiciatis commodi animi, mollitia minus at alias voluptatum maxime aliquam voluptates velit laborum aspernatur sapiente maiores possimus adipisci voluptate aut quidem distinctio. Molestias assumenda quae et, exercitationem dolores quia vitae non nulla corporis cupiditate impedit. Libero ab eum quo rerum provident, consequuntur sunt ad eveniet totam ut nemo laboriosam. Facilis voluptate quisquam reiciendis, sunt sint veritatis. Doloremque harum et cupiditate. Similique eaque soluta eos! Autem, culpa iste. Quod possimus doloribus repudiandae repellendus mollitia quos eaque accusamus at facilis earum eos unde in recusandae voluptatum eligendi corrupti, dolorum perspiciatis provident explicabo ad tenetur voluptatibus ipsam perferendis suscipit? Architecto, temporibus molestias dolor et, nostrum corporis eos inventore assumenda deleniti ullam tenetur suscipit nihil cumque doloremque maiores provident vero velit. Numquam vel ad magnam quibusdam veritatis distinctio alias eaque iure odit consequatur? Consequuntur voluptates adipisci, reiciendis debitis beatae nemo deleniti quibusdam molestiae repudiandae recusandae nobis repellat blanditiis magnam maxime suscipit fuga doloribus!
+
+<!-- footer -->
+<?php include "./components/footer.php"; ?>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
